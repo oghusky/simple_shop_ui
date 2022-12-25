662fce3cfbc2b6ede368a7d9b17c64269ae9c1b1
oghusky <philiposimmons@gmail.com>
2022-12-25 17:39:03 -0500

    able to conditionally render nav

--- src/components/Headers/MainNav.js
+++ src/components/Headers/MainNav.js
@@ -1,7 +1,13 @@
-import React from 'react'
-
+import HomePageNav from './HomePageNav'
+import StoreNav from './StoreNav'
 export default function MainNav() {
+
+  const pathname = window.location.pathname;
+  console.log(pathname);
+  if(pathname.includes("/store")|| pathname.includes("/product")){
+    return <StoreNav/>
+  }
   return (
-    <div>MainNav</div>
+    <HomePageNav/>
   )
 }
---
+++ src/components/Headers/StoreNav.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StoreNav() {
+  return (
+    <div>StoreNav</div>
+  )
+}
